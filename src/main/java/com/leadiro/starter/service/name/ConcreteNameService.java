c240d46cd68cf66b3fcc4ca67490431d51b31604
owenrb <owenrb@localhost>
2020-08-12 01:42:47 +0800

    work/unit-test on name service

--- src/main/java/com/leadiro/starter/service/name/ConcreteNameService.java
+++ src/main/java/com/leadiro/starter/service/name/ConcreteNameService.java
@@ -15,8 +15,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ConcreteNameService implements NameService {
 	
-	private List<String> preSurnames = Arrays.asList("del");
-	private List<String> titles = Arrays.asList("csar", "dr", "rev", "aka", "mba", "bsc" );
+	private List<String> preSurnames = Arrays.asList("Del");
+	private List<String> titles = Arrays.asList("csar", "dr", "rev", "mba", "bsc" , "ii", "jr", "msc", "mph", "diped", "maca", "phd");
 	
 	@Override
 	public String[] process(String name) {
@@ -27,13 +27,102 @@ public class ConcreteNameService implements NameService {
 			return null;
 		}
 		
+		// search for comma
+		int idx = name.indexOf(",");
+		if(idx >= 0) {
+			String left = idx == 0? "" : name.substring(0, idx);
+			String right = name.substring(idx+1);
+			
+			return process(right + " " + left);
+		}
 		
-		List<String> list = Stream.of(name.split("\\s+"))
-				.map(elem -> new String(elem))
+		
+		List<String> list = Stream.of(name.trim().split("\\s+"))
+				.filter(title -> !isSalutation(title))
+				.filter(title -> !hasEnclosings(title))
+				.filter(title -> !tooShort(title))
+				.takeWhile(aka -> !isAka(aka))
+				.filter(alpha -> isAlpha(alpha))
+				.map(elem -> toTitle(elem))
 				.collect(Collectors.toList());
 		
+		list = processCompoundSurnames(list);
 		
-		return list.toArray(new String[] {});
+		
+		int size = list.size();
+		if(size < 2) {
+			return null;
+		}
+		
+		return new String[] {list.get(0), list.get(size - 1)};
+	}
+	
+	private boolean isAlpha(String alpha) {
+		return alpha.matches("[a-zA-Z]+");
+	}
+
+	private boolean tooShort(String title) {
+		
+		return title.replaceAll("\\.", "").length() < 2;
+	}
+
+	private boolean isAka(String aka) {
+		
+		return "aka".equalsIgnoreCase(aka.replaceAll("\\.", ""));
+	}
+
+	private boolean hasEnclosings(String name) {
+		return isEnclosing(name, '\"')
+				|| isEnclosing(name, '\'')
+				|| isEnclosing(name, '(', ')');
+	}
+	
+	private boolean isEnclosing(String name, char first) {
+		return isEnclosing(name, first, first);
+	}
+	
+	private boolean isEnclosing(String name, char first, char last) {
+		int len = name.length();
+		return name.charAt(0) == first && name.charAt(len - 1) == last;
+	}
+
+	private boolean isSalutation(String title) {
+		String key = title.replaceAll("\\.", "").toLowerCase();
+		return titles.contains(key);
+	}
+	
+	private List<String> processCompoundSurnames(List<String> list) {
+		
+		int size = list.size();
+		if(size > 1) {
+			String subj = list.get(size - 2);
+			if(preSurnames.contains(subj)) {
+				String last = list.get(size - 1);
+				
+				List<String> newList = list.subList(0, size-2); // split
+				newList.add(subj + " " + last);					// and combine
+				
+				return processCompoundSurnames(newList);	// recursion here
+				
+			}
+		}
+		
+		return list;
+	}
+
+	/**
+	 * Convert non-empty string to title format
+	 * @param name
+	 * @return
+	 */
+	private String toTitle(String name) {
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(Character.toUpperCase(name.charAt(0)));
+		sb.append(name.substring(1).toLowerCase());
+		
+		return sb.toString();
+		
 	}
 
 }
